bfeda330ac0b90d2020f20c091a40fce1f4a9be0
Lewbagz <lewishardie@gmail.com>
2023-11-30 20:53:29 +1100

    nav, set timeout, routes

--- src/NumberGrabber.jsx
+++ src/NumberGrabber.jsx
@@ -1,14 +1,30 @@
-import { useParams } from "react-router-dom"
+import { useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom"
 
 export default function NumberGrabber(){
     
     // const params = useParams();
     const {banana, id, potato} = useParams();
+    const navigate = useNavigate();
+
+    function navigateToHome(){
+        console.log("The logic that posts a message, sbumits a form or whatever")
+        navigate('/');
+    }
+
+    useEffect(()=> {
+        setTimeout(() => {
+            console.log("Simulating we're doing something")
+            navigate('/');
+        }, 5000)
+    })
+
     return(
         <div>
             <h1>The number that this component grabbed is: {banana}</h1>
             <h1>and the id is: {id}</h1>
             <h1>and potatoes: {potato}</h1>
+            <button onClick={navigateToHome}>Home</button>
         </div>
     )
 }
